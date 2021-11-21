b3195fa39e413990e90829ade83302a1fbb184a7
pauls <pauls.bebris@gmail.com>
2021-11-21 17:10:34 +0200

    numbers as simple functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import "./app.css";
 
 const App = () => {
-  return <div>App</div>;
+  const cardSize = 24;
+  const numbersSource = [];
+  let numbers = [];
+
+  const fillNumbersSource = () => {
+    for (let index = 1; index <= cardSize; index++) {
+      numbersSource.push(index);
+    }
+  };
+  fillNumbersSource();
+
+  const scrambleNumbers = () => {
+    while (numbersSource.length > 0) {
+      const possition = Math.floor(Math.random() * numbersSource.length);
+      numbers.push(numbersSource[possition]);
+      numbersSource.splice(possition, 1);
+    }
+  };
+  scrambleNumbers();
+
+  const renderGrid = () => {
+    return numbers.map((number, i) => {
+      return (
+        <div className="number" key={i}>
+          {number}
+        </div>
+      );
+    });
+  };
+
+  return <div className="app">{renderGrid()}</div>;
 };
 
 export default App;
